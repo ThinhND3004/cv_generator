a4fd2a38ab9b745e749a2e713ce0dddbcb794222
Vũ Hoàng Hiếu Ngân <nganvhhse183096@fpt.edu.vn>
2024-11-04 10:37:27 +0700

    create Template Project
    create new window Template1

---
+++ Template/Template1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Template
+{
+    /// <summary>
+    /// Interaction logic for Template1.xaml
+    /// </summary>
+    public partial class Template1 : Window
+    {
+        public Template1()
+        {
+            InitializeComponent();
+        }
+    }
+}
